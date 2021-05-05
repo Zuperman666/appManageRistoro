e83b8bb1fe113a5717499998295b2388470f2e56
manuel <manuel.fia@synclab.net>
2021-05-05 21:32:07 +0200

    bozza scheda ordine

--- lib/pages/ordine.dart
+++ lib/pages/ordine.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sala/model/interface.dart';
 import 'package:flutter_sala/widget/alertDelete.dart';
+import 'package:flutter_sala/widget/spinbox.dart';
 
 class Order extends StatelessWidget {
   final Data data;
-  Order(this.data,{Key key}) : super(key: key);
+  Order(this.data, {Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,39 +17,39 @@ class Order extends StatelessWidget {
           noOnPressed: () {},
           yes: "Si",
           no: "No");
-      showDialog(context: context, builder: (BuildContext context) => baseDialog);
+      showDialog(
+          context: context, builder: (BuildContext context) => baseDialog);
     }
 
     return Scaffold(
-      appBar: AppBar(
-          title: Text('Tavolo ${data.name}'),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.more_vert),
-              tooltip: 'Resettare Tavolo',
-              onPressed: () {
-                _confirmRegister();
-              },
-            ),
-          ]),
-      body: Center (
-        child:Row(
-        children: [
-          Container(
-            padding: EdgeInsets.all(80.0),
-            child: ElevatedButton(
-              child: Text('Invia'),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/catalog');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.yellow,
+        appBar: AppBar(
+            title: Text('Tavolo ${data.name}'),
+            centerTitle: true,
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.more_vert),
+                tooltip: 'Resettare Tavolo',
+                onPressed: () {
+                  _confirmRegister();
+                },
               ),
-            ),
-          )
-        ],
-      ),
-    ));
+            ]),
+        body: Column(
+          children: [
+            SpinBox("test", 5),
+            Container(
+              padding: (const EdgeInsets.symmetric(vertical: 30.0)),
+              child: TextButton(
+                onPressed: () => {print('mandaconto')},
+                child: Text('Invia Ordine'),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.blue,
+                  padding: (const EdgeInsets.symmetric(horizontal: 10.0)),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
---
+++ lib/widget/spinbox.dart
@@ -0,0 +1,122 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_sala/model/interface.dart';
+
+class SpinBox extends StatefulWidget {
+  final String name;
+  final int quantita;
+  SpinBox(this.name, this.quantita, {Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<SpinBox> {
+
+
+
+  String _name;
+  int _quantita;
+  bool showModal = false;
+  final myController = TextEditingController();
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the widget tree.
+    // This also removes the _printLatestValue listener.
+    myController.dispose();
+    super.dispose();
+  }
+  void change() {
+    print(showModal);
+    setState(() {
+      showModal = !showModal;
+    });
+  }
+  _printLatestValue() {
+    print("Second text field: ${myController.text}");
+  }
+
+  @override
+  void initState() {
+    myController.addListener(_printLatestValue);
+    super.initState();
+    _name = widget.name;
+    _quantita = widget.quantita;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: (const EdgeInsets.symmetric(vertical: 10.0)),
+            child: new Align(
+              alignment: Alignment
+                  .center, // Align however you like (i.e .centerRight, centerLeft)
+              child: RichText(
+                text: TextSpan(
+                    text: _name.toUpperCase(),
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 20)),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          )
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TextButton(
+            child: Icon(Icons.remove),
+            onPressed: () => {_quantita--},
+          ),
+          RichText(
+            text: TextSpan(
+                text: _quantita.toString().toUpperCase(),
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                    fontSize: 16)),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            child: Icon(Icons.add),
+            onPressed: () => {_quantita++},
+          )
+        ],
+      ),
+      Visibility(
+          visible: (showModal == false),
+          child: Row(
+            children: [
+              new Flexible(
+                  child: TextButton(
+                    child: Icon(Icons.note),
+                    onPressed: change,
+                  )),
+            ],
+          )),
+      Visibility(
+        visible: (showModal == true),
+          child: Row(
+        children: [
+          new Flexible(
+              child: new TextField(
+            controller: myController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Note',
+            ),
+          )),
+        ],
+      ))
+    ]);
+  }
+}
